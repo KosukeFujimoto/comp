34c0f160c0ed1d806ea3c361799e629f51579bff
KosukeFujimoto <kosuke.fujimoto3@gmail.com>
2021-12-11 15:01:12 +0900

    added sample data and tab and table

--- node/csv/src/App.js
+++ node/csv/src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col, Tabs, Tab, Table, thread, tbody, tr, td } from 'react-bootstrap'
+
+
+// Generate Table Based on input csv file
+// Add function to check if 
+function DataTable() {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+
+function Main() {
+
+  return (<Tabs
+    defaultActiveKey="home"
+    id="noanim-tab-example"
+    className="mb-3"
+  >
+    {/* 
+      Generate Tabs based on a config file 
+    */}
+    <Tab eventKey="home" title="Home">
+      <DataTable />
+    </Tab>
+  </Tabs>);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        {/* Left Column should have Menu such as load file, export file */}
+        <Col md={3}></Col>
+        {/* Main Part will load csv file from local directory and populate config page */}
+        <Col md={6}>
+          <Main />
+        </Col>
+        {/* No usage for nows */}
+        <Col md={3}></Col>
+      </Row>
+    </Container >
   );
 }
 
